d34b804495cb5a9f3abc9eb411d8bfb8e0428142
MizzieWiller <anacsilvabarra@gmail.com>
2026-09-09 20:44:02 -0300

    Update funções tabuada e cubo.cpp

--- 3° Ciclo/Estrutura de Dados/aula09-09/funções tabuada e cubo.cpp
+++ 3° Ciclo/Estrutura de Dados/aula09-09/funções tabuada e cubo.cpp
@@ -1,1 +1,29 @@
+#include <iostream>
+using namespace std;
+
+    void tabuada(int numero) {
+        cout << "\n Tabuada do " << numero << endl;
+      for (int i = 1; i <= 10; i++) {
+        cout << numero << " x " << i << " = " << (numero * i) << endl;
+  }
+}
+
+    int calcularCubo(int numero) {
+    return numero * numero * numero;
+}
+
+int main() {
+  int num;
+
+  cout << "\n informe um numero inteiro:" << endl;
+  cin >> num;
+
+  tabuada(num);
+
+  int resultadoCubo = calcularCubo(num);
+      cout << "\n O cubo de " << num << "eh: " << resultadoCubo << endl;
+
+  return 0;
+}
+  
 
